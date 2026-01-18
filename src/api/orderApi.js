fe96706294c2511a8b7e31ef9bff8d9432f55baa
Ashish959 <rajc01997@gmail.com>
2026-01-18 17:13:53 +0530

    Migrate order table to MUI DataGrid

--- src/api/orderApi.js
+++ src/api/orderApi.js
@@ -9,19 +9,18 @@ export const getOrderByClient = async ({
   division = "",
 }) => {
   const payload = {
+    Action: action,
+    Division: division,
     Emp_Id: localStorage.Emp_Id,
     Order_Id: clientId,
     StartDate: dayjs(startDate).format("MM/DD/YYYY"),
     EndDate: dayjs(endDate).format("MM/DD/YYYY"),
-    SessionId: "",
-    Action: action,
-    Division: division,
+    SessionId: "ZTdNGDPPgfwHxKLSE673SJIXW6rcm4JcOHWp25r1r8kKYCYHkb41CWUCHF8SND/VP7DbKrDXEXgOneeUH0yBdQ==", 
   };
 
   const res = await axios.post(
     "/api/Order/GetOrderbyClient",
     payload
   );
-
   return res.data;
 };
--- src/store.js
+++ src/store.js
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
+import authReducer from "./features/auth/authSlice";
+import dashboardOrdersReducer from "./features/dashboard/dashboardOrdersSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, } from "redux-persist";
+
+const authPersistConfig = { key: "auth", storage, };
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
-  reducer: {
+  reducer: { 
     counter: counterReducer,
-  },
+    auth: persistedAuthReducer, 
+    dashboardOrders:dashboardOrdersReducer,
+    },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: { ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER,], }, }),
 });
+
+export const persistor = persistStore(store);
+
